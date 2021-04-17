55a635b5505c22388f11c92c74382b585b4f8e67
Ryota-takei <wlol0747@yahoo.co.jp>
2021-04-17 11:26:25 +0900

    リファクタリング

--- src/components/templates/SearchArea/SearchArea.tsx
+++ src/components/templates/SearchArea/SearchArea.tsx
@@ -1,4 +1,4 @@
-import { memo, useCallback, useState } from "react";
+import { memo,  useState } from "react";
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
 import { Button, Container, Input } from "@chakra-ui/react";
